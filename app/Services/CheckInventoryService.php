c71acca144ae2048ca22ba2a0df782e6b588259c
thangngo.dev <thangngo130802@gmail.com>
2024-07-11 09:42:35 +0700

    update_kho

--- app/Services/CheckInventoryService.php
+++ app/Services/CheckInventoryService.php
@@ -3,6 +3,8 @@
 namespace App\Services;
 
 use App\Models\CheckInventory;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 
 
@@ -17,6 +19,36 @@ class CheckInventoryService
     public function __construct(CheckInventory $checkInventory)
     {
         $this->checkInventory = $checkInventory;
+    }
+
+    public function getAllCheckInventory() : \Illuminate\Database\Eloquent\Collection
+    {
+
+        try {
+            $checkInventory = $this->checkInventory->all();
+            return $checkInventory;
+        } catch (Exception $e) {
+            Log::error('Failed to get all checkInventory: ' . $e->getMessage());
+            throw new Exception('Failed to get all checkInventory');
+        }
+    }
+
+    /**
+     * Summary of getCheckInventoryById
+     * @param mixed $id
+     * @throws Exception
+     * @return CheckInventory
+     */
+    public function getCheckInventoryById($id): CheckInventory
+    {
+
+        try {
+            $checkInventory = $this->checkInventory->find($id);
+            return $checkInventory;
+        } catch (Exception $e) {
+            Log::error('Failed to get  checkInventory by '. $id. '-'. $e->getMessage());
+            throw new Exception('Failed to get  checkInventory by ' .$id);
+        }
     }
 
 
