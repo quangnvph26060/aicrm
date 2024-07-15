3341fe0458c3e015c9180a00f12dcc140f0b11d6
thangngo.dev <thangngo130802@gmail.com>
2024-07-15 17:19:32 +0700

    fix conflict

--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,5 @@
 b<?php
 
-<<<<<<< HEAD
-    use App\Http\Controllers\Admin\AdminController;
-    use App\Http\Controllers\Admin\BrandController;
-    use App\Http\Controllers\Admin\CategorieController;
-    use App\Http\Controllers\Admin\CheckInventoryController;
-    use App\Http\Controllers\Admin\ClientController;
-    use App\Http\Controllers\Admin\DashboardController;
-    use App\Http\Controllers\Admin\OrderController;
-    use App\Http\Controllers\Admin\ProductController;
-    use App\Http\Controllers\Admin\UserController;
-    use App\Http\Controllers\AuthController;
-    use App\Http\Controllers\PaymentController;
-    use App\Http\Controllers\Staff\ClientController as StaffClientController;
-    use App\Http\Controllers\Staff\OrderController as StaffOrderController;
-    use App\Http\Controllers\Staff\ProductController as StaffProductController;
-    use App\Http\Controllers\Admin\ConfigController;
-    use App\Http\Controllers\Admin\SupplierController;
-    use App\Http\Controllers\Client\SignUpController;
-    use App\Http\Controllers\Staff\CheckInventoryController as staffcheckController;
-    use App\Http\Controllers\Staff\WareHomeController;
-    use App\Http\Controllers\SuperAdmin\StoreController;
-    use App\Models\Categories;
-    use App\Http\Middleware\CheckLogin;
-use App\Models\Supplier;
-use Illuminate\Support\Facades\Route;
-
-    // Route::get('', [CategorieController::class, 'index']);
-    Route::get('/register', [SignUpController::class, 'index'])->name('register.index');
-    Route::post('/register_account', [SignUpController::class, 'store'])->name('register.signup');
-    Route::get('/', function () {
-        return view('auth.login');
-    })->name('formlogin');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::get('/verify-otp', [AuthController::class, 'showVerifyOtp'])->name('verify-otp');
-    Route::post('/verify-otp', [AuthController::class, 'verifyOtp'])->name('verify_otp_confirm');
-    Route::middleware(['auth'])->group(function () {
-        Route::get('/home', function () {
-            return view('home');
-        })->name('home');
-        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-        Route::get('/payment', [PaymentController::class, 'showPaymentForm'])->name('payment.form');
-        Route::post('/payment', [PaymentController::class, 'processPayment'])->name('payment.process');
-        Route::get('/payment-success', [PaymentController::class, 'paymentSuccess'])->name('payment.success');
-        Route::post('/payment-notify', [PaymentController::class, 'paymentNotify'])->name('payment.notify');
-=======
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CategorieController;
@@ -124,7 +79,6 @@ Route::middleware(CheckLogin::class)->prefix('admin')->name('admin.')->group(fun
         Route::post('product-category', [ProductController::class, 'Changecategory'])->name('changecategory');
         Route::post('product-status', [ProductController::class, 'Changestatus'])->name('changestatus');
         Route::get('search/name', [ProductController::class, 'findByName'])->name('findName');
->>>>>>> 3764bace693126fbda986ca1eb27a17b42ddd749
     });
     Route::get('forget-password', function () {
         return view('auth.forget-password');
