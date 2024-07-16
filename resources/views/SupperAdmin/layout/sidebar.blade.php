7064895396d1d8c39c64ae160afe5b85a2e74db3
thangngo.dev <thangngo130802@gmail.com>
2024-07-16 10:49:14 +0700

    update _ supperadmin_login

---
+++ resources/views/SupperAdmin/layout/sidebar.blade.php
@@ -0,0 +1,50 @@
+<div class="sidebar" data-background-color="dark">
+    <div class="sidebar-logo">
+        <!-- Logo Header -->
+        <div class="logo-header" data-background-color="dark">
+            <a href="{{ route('admin.dashboard') }}" class="logo">
+                <img src="{{ asset('assets/img/kaiadmin/logo_light.svg') }}" alt="navbar brand" class="navbar-brand"
+                    height="20" />
+            </a>
+            <div class="nav-toggle">
+                <button class="btn btn-toggle toggle-sidebar">
+                    <i class="gg-menu-right"></i>
+                </button>
+                <button class="btn btn-toggle sidenav-toggler">
+                    <i class="gg-menu-left"></i>
+                </button>
+            </div>
+            <button class="topbar-toggler more">
+                <i class="gg-more-vertical-alt"></i>
+            </button>
+        </div>
+        <!-- End Logo Header -->
+    </div>
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <ul class="nav nav-secondary">
+                <li class="nav-item active">
+                    <a href="{{ route('admin.dashboard') }}">
+                        <i class="fas fa-tachometer-alt"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li class="nav-section">
+                    <span class="sidebar-mini-icon">
+                        <i class="fa fa-ellipsis-h"></i>
+                    </span>
+                    <h4 class="text-section">Thành phần quản lý</h4>
+                </li>
+
+
+                <li class="nav-item">
+                    <a href="{{ route('sa.store.index') }}">
+                        <i class="fas fa-shopping-cart"></i>
+                        <p>Cửa hàng</p>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</div>
