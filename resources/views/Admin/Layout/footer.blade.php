8e5e90139df1ee19f59570a24937a2a072d1b84f
dev_phongpt <phamtuanphongisme@gmail.com>
2024-07-16 09:32:13 +0700

    update layout

--- resources/views/Admin/Layout/footer.blade.php
+++ resources/views/Admin/Layout/footer.blade.php
@@ -1,27 +1,27 @@
 <footer class="footer">
     <div class="container-fluid d-flex justify-content-between">
-      <nav class="pull-left">
-        <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link" href="3">
+        <nav class="pull-left">
+            <ul class="nav">
+                <li class="nav-item">
+                    {{-- <a class="nav-link" href="3">
               ThemeKita
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#"> Help </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#"> Licenses </a>
-          </li>
-        </ul>
-      </nav>
-      <div class="copyright">
-        2024, made with <i class="fa fa-heart heart text-danger"></i> by
-        <a href="">ThemeKita</a>
-      </div>
-      <div>
-        Distributed by
-        <a target="_blank" href="#">ThemeWagon</a>.
-      </div>
+            </a> --}}
+                </li>
+                <li class="nav-item">
+                    {{-- <a class="nav-link" href="#"> Help </a> --}}
+                </li>
+                <li class="nav-item">
+                    {{-- <a class="nav-link" href="#"> Licenses </a> --}}
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright">
+            Copyright 2024 ©
+            <a href="{{ config('app.url') }}">AICRM.vn</a>
+        </div>
+        <div>
+            {{-- Distributed by
+        <a target="_blank" href="#">ThemeWagon</a>. --}}
+        </div>
     </div>
-  </footer>
+</footer>
--- resources/views/auth/forget-password.blade.php
+++ resources/views/auth/forget-password.blade.php
@@ -1,25 +1,25 @@
 @extends('Themes.main.main')
 
 @section('content')
-<div class="bg-overlay"></div>
-<!-- auth-page content -->
-<div class="auth-page-content overflow-hidden pt-lg-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card overflow-hidden">
-                    <div class="row justify-content-center g-0">
-                        <div class="col-lg-6">
-                            <div class="p-lg-5 p-4 auth-one-bg h-100">
-                                <div class="bg-overlay"></div>
-                                <div class="position-relative h-100 d-flex flex-column">
-                                    <div class="mb-4">
-                                        <a href="index.html" class="d-block">
-                                            <img src="assets/images/logo-light.png" alt="" height="18">
-                                        </a>
-                                    </div>
-                                    <div class="mt-auto">
-                                        <div class="mb-3">
+    <div class="bg-overlay"></div>
+    <!-- auth-page content -->
+    <div class="auth-page-content overflow-hidden pt-lg-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card overflow-hidden">
+                        <div class="row justify-content-center g-0">
+                            <div class="col-lg-6">
+                                <div class="p-lg-5 p-4 auth-one-bg h-100">
+                                    <div class="bg-overlay"></div>
+                                    <div class="position-relative h-100 d-flex flex-column">
+                                        <div class="mb-4">
+                                            <a href="index.html" class="d-block">
+                                                <img src="assets/images/logo-light.png" alt="" height="18">
+                                            </a>
+                                        </div>
+                                        <div class="mt-auto">
+                                            {{-- <div class="mb-3">
                                             <i class="ri-double-quotes-l display-4 text-success"></i>
                                         </div>
 
@@ -31,64 +31,64 @@
                                             </div>
                                             <div class="carousel-inner text-center text-white-50 pb-5">
                                                 <div class="carousel-item active">
-                                                    <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
                                                 </div>
                                                 <div class="carousel-item">
-                                                    <p class="fs-15 fst-italic">" The theme is really great with an amazing customer support."</p>
                                                 </div>
                                                 <div class="carousel-item">
-                                                    <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
                                                 </div>
                                             </div>
+                                        </div> --}}
+                                            <!-- end carousel -->
                                         </div>
-                                        <!-- end carousel -->
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <!-- end col -->
+                            <!-- end col -->
 
-                        <div class="col-lg-6">
-                            <div class="p-lg-5 p-4">
-                                <h5 class="text-primary">Forgot Password?</h5>
-                                <p class="text-muted">Reset password with velzon</p>
+                            <div class="col-lg-6">
+                                <div class="p-lg-5 p-4">
+                                    <h5 class="text-primary">Quên mật khẩu?</h5>
+                                    <p class="text-muted">Đặt lại mật khẩu</p>
 
-                                <div class="mt-2 text-center">
-                                    <lord-icon
-                                        src="https://cdn.lordicon.com/rhvddzym.json" trigger="loop" colors="primary:#0ab39c" class="avatar-xl">
-                                    </lord-icon>
-                                </div>
+                                    <div class="mt-2 text-center">
+                                        <lord-icon src="https://cdn.lordicon.com/rhvddzym.json" trigger="loop"
+                                            colors="primary:#0ab39c" class="avatar-xl">
+                                        </lord-icon>
+                                    </div>
 
-                                <div class="alert border-0 alert-warning text-center mb-2 mx-2" role="alert">
-                                    Nhập email của bạn và hướng dẫn sẽ được gửi cho bạn!
-                                </div>
-                                <div class="p-2">
-                                    <form>
-                                        <div class="mb-4">
-                                            <label class="form-label">Email</label>
-                                            <input type="email" class="form-control" id="email" placeholder="Enter email address">
-                                        </div>
+                                    <div class="alert border-0 alert-warning text-center mb-2 mx-2" role="alert">
+                                        Nhập email của bạn và hướng dẫn sẽ được gửi cho bạn!
+                                    </div>
+                                    <div class="p-2">
+                                        <form>
+                                            <div class="mb-4">
+                                                <label class="form-label">Email</label>
+                                                <input type="email" class="form-control" id="email"
+                                                    placeholder="Enter email address">
+                                            </div>
 
-                                        <div class="text-center mt-4">
-                                            <button class="btn btn-success w-100" type="submit">Send Reset Link</button>
-                                        </div>
-                                    </form><!-- end form -->
-                                </div>
+                                            <div class="text-center mt-4">
+                                                <button class="btn btn-success w-100" type="submit">Gửi lại mã</button>
+                                            </div>
+                                        </form><!-- end form -->
+                                    </div>
 
-                                <div class="mt-5 text-center">
-                                    <p class="mb-0">Wait, I remember my password... <a href="{{route('login')}}" class="fw-semibold text-primary text-decoration-underline"> Click here </a> </p>
+                                    <div class="mt-5 text-center">
+                                        <p class="mb-0"> <a href="{{ route('formlogin') }}"
+                                                class="fw-semibold text-primary text-decoration-underline"> Đăng nhập </a>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
+                            <!-- end col -->
                         </div>
-                        <!-- end col -->
+                        <!-- end row -->
                     </div>
-                    <!-- end row -->
+                    <!-- end card -->
                 </div>
-                <!-- end card -->
-            </div>
-            <!-- end col -->
+                <!-- end col -->
 
+            </div>
+            <!-- end row -->
         </div>
-        <!-- end row -->
-    </div>
-@endsection
+    @endsection
