49bb09ebdd97a37a2bb8a8ab6bdd9dd8fe013282
dev_phongpt <phamtuanphongisme@gmail.com>
2024-07-23 08:28:07 +0700

    sửa phiếu chi

--- app/Http/Controllers/Staff/ClientController.php
+++ app/Http/Controllers/Staff/ClientController.php
@@ -8,6 +8,7 @@ use App\Models\Cart;
 use App\Models\Config;
 use App\Models\Order;
 use App\Models\OrderDetail;
+use App\Models\ReceiptDetail;
 use App\Services\ClientService;
 use App\Services\DebtKHService;
 use App\Services\ProductService;
@@ -50,11 +51,10 @@ class ClientController extends Controller
             $listphone = $this->clientService->getAllClientStaff()->pluck('phone');
             if ($listphone->contains($request->phone)) {
                 return redirect()->back()->with('fail', 'Khách hàng đã tồn tại');
-            }else{
+            } else {
                 $client = $this->clientService->addClient($request->all());
                 return redirect()->back()->with('action', 'Thêm khách hàng thành công');
             }
-
         } catch (Exception $e) {
             Log::error('Failed to fetch clients: ' . $e->getMessage());
             return ApiResponse::error('Failed to fetch clients', 500);
@@ -79,7 +79,8 @@ class ClientController extends Controller
     }
 
 
-    public function bill($file){
+    public function bill($file)
+    {
         $response = Response::download(public_path($file))->deleteFileAfterSend(true);
 
         return $response;
@@ -91,7 +92,7 @@ class ClientController extends Controller
             $listphone = $this->clientService->getAllClientStaff()->pluck('phone');
             $cartItems = Cart::where('user_id', $user->id)->get();
             $sum = 0;
-            $client= array();
+            $client = array();
             $trangthai = $request->status;
             foreach ($cartItems as $key => $item) {
                 $sum += $item->product->priceBuy * $item->amount;
@@ -129,36 +130,67 @@ class ClientController extends Controller
                         'product_id' => $item->product_id
                     ]);
                 }
-
             }
 
-            if($trangthai != 4){
+            if ($trangthai != 4) {
+                $listreceipt = $this->receiptsService->getAllReceipts()->pluck('client_id');
+                if ($listreceipt->contains($client->id)) {
+                    $receipt = $this->receiptsService->findRecieptByClient($client->id);
+                    $data1 = [
+                        'amount_spent' => $sum + $receipt->amount_spent,
+                        'date_spent' => Carbon::now()->toDateString()
+                    ];
+                    $receipt->updateClientDebt($data1);
+                    $detail = [
+                        'receipt_id' => $receipt->id,
+                        'content' => 'Thu từ khách hàng có số điện thoại ' . $request->phone,
+                        'amount' => $sum,
+                        'date' => Carbon::now()->toDateString()
+                    ];
+                    ReceiptDetail::create($detail);
+                } else {
+                    $data1 = [
+                        'client_id' => $client->id,
+                        'content' => 'Thu từ khách hàng có số điện thoại ' . $request->phone,
+                        'amount_spent' => $sum,
+                        'date_spent' => Carbon::now()->toDateString()
+                    ];
+                    $receipt = $this->receiptsService->addReceipts($data1);
+                    $detail = [
+                        'receipt_id' => $receipt->id,
+                        'content' => 'Thu từ khách hàng có số điện thoại ' . $request->phone,
+                        'amount' => $sum,
+                        'date' => Carbon::now()->toDateString()
+                    ];
+                    ReceiptDetail::create($detail);
+                }
+
                 $data1 = [
-                    'content' => 'Thu từ khách hàng có số điện thoại '. $request->phone,
+                    'content' => 'Thu từ khách hàng có số điện thoại ' . $request->phone,
                     'amount_spent' => $sum,
                     'date_spent' => Carbon::now()->toDateString()
                 ];
                 $this->receiptsService->addReceipts($data1);
-            }else{
+            } else {
                 $ClientDebt = $this->debtKHService->getAllClientDebt()->pluck('client_id');
-                if($ClientDebt->contains($client->id)){
+                if ($ClientDebt->contains($client->id)) {
                     $clientdebt = $this->debtKHService->findClientDebtByClient($client->id);
                     $data2 = [
                         'amount' => $client->amount + $sum,
                     ];
                     $this->debtKHService->updateClientDebt($data2, $client->id);
-                }else{
+                } else {
                     $data = [
                         'client_id' => $client->id,
                         'amount' => $sum,
-                        'description' => 'Khách có số điện thoại '.$request->phone
+                        'description' => 'Khách có số điện thoại ' . $request->phone
                     ];
                     $this->debtKHService->addClientDebt($data);
                 }
             }
             Cart::where('user_id', $user->id)->delete();
             $config = Config::first();
-            $html = view('Themes.pages.bill.index', compact('cartItems','sum', 'client', 'user', 'config'))->render();
+            $html = view('Themes.pages.bill.index', compact('cartItems', 'sum', 'client', 'user', 'config'))->render();
             $pdf = Pdf::loadHTML($html);
             $pdfFileName = 'order.pdf';
             $pdf->save(public_path($pdfFileName));
